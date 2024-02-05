83446cf054d666b3bbca0c9da4635cf9c695da28
sunil <sunilyadav770275@gmail.com>
2024-02-05 16:37:41 +0530

    'commit'

---
+++ src/main/java/com/example/MavanApp1/MavanApp1Application.java
@@ -0,0 +1,13 @@
+package com.example.MavanApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavanApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavanApp1Application.class, args);
+	}
+
+}
